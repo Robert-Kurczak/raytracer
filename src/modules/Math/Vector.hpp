140fa5d78df5ea208689b7370fa6a5a475624927
Robert Kurczak <robertkurczak.studia@gmail.com>
2026-03-03 04:19:07 +0100

    Added vector math functions

--- src/modules/Math/Vector.hpp
+++ src/modules/Math/Vector.hpp
@@ -1,48 +1,202 @@
 #pragma once
 
+#include <algorithm>
 #include <array>
-#include <cstdint>
+#include <cmath>
 #include <initializer_list>
+#include <ostream>
 
 namespace RTC {
-template<typename Type, uint8_t Size>
+template<typename Type, size_t Size>
 class Vector {
 public:
     std::array<Type, Size> dimensions {};
 
-    constexpr Vector() = default;
+    constexpr Vector() noexcept = default;
 
-    constexpr Vector(std::initializer_list<Type> list) {
-        for (uint8_t i = 0; i < Size; i++) {
+    constexpr Vector(std::initializer_list<Type> list) noexcept {
+        const size_t elementsAmount = std::min(Size, list.size());
+
+        for (size_t i = 0; i < elementsAmount; i++) {
             dimensions[i] = list.begin()[i];
         }
     }
 
-    constexpr Type& operator[](uint32_t index) noexcept {
+    [[nodiscard]] constexpr Type& operator[](size_t index) noexcept {
         return dimensions[index];
     }
-    constexpr const Type& operator[](uint32_t index) const noexcept {
+    [[nodiscard]] constexpr const Type& operator[](
+        size_t index
+    ) const noexcept {
         return dimensions[index];
     }
 
-    [[nodiscard]] constexpr Type dot(
+    Vector<Type, Size>& operator+=(
+        const Vector<Type, Size>& vector
+    ) noexcept {
+        for (size_t i = 0; i < Size; i++) {
+            dimensions[i] += vector[i];
+        }
+
+        return *this;
+    }
+
+    Vector<Type, Size>& operator-=(
+        const Vector<Type, Size>& vector
+    ) noexcept {
+        for (size_t i = 0; i < Size; i++) {
+            dimensions[i] -= vector[i];
+        }
+
+        return *this;
+    }
+
+    Vector<Type, Size>& operator*=(const Type& scalar) noexcept {
+        for (size_t i = 0; i < Size; i++) {
+            dimensions[i] *= scalar;
+        }
+
+        return *this;
+    }
+
+    Vector<Type, Size>& operator/=(const Type& scalar) noexcept {
+        for (size_t i = 0; i < Size; i++) {
+            dimensions[i] /= scalar;
+        }
+
+        return *this;
+    }
+
+    [[nodiscard]] constexpr Vector<Type, Size> operator-() const noexcept {
+        Vector<Type, Size> resultVector {};
+
+        for (size_t i = 0; i < Size; i++) {
+            resultVector[i] = -dimensions[i];
+        }
+
+        return resultVector;
+    }
+
+    [[nodiscard]] constexpr Type getDotProduct(
         const Vector<Type, Size>& vector
     ) const noexcept {
         Type result {0};
-        for (uint8_t i = 0; i < Size; i++) {
+        for (size_t i = 0; i < Size; i++) {
             result += dimensions[i] * vector[i];
         }
         return result;
     }
+
+    [[nodiscard]] constexpr Type getSquaredLength() const noexcept {
+        Type result {};
+        for (size_t i = 0; i < Size; i++) {
+            result += dimensions[i] * dimensions[i];
+        }
+        return result;
+    }
+
+    [[nodiscard]] constexpr Type getLength() const noexcept {
+        return std::sqrt(getSquaredLength());
+    }
+
+    [[nodiscard]] constexpr Vector<Type, Size> getVersor() const noexcept {
+        return *this / getLength();
+    }
 };
+
+template<typename Type, size_t Size>
+constexpr std::ostream& operator<<(
+    std::ostream& out,
+    const Vector<Type, Size>& vector
+) noexcept {
+    out << "Vector<" << int(Size) << "> (";
+
+    for (size_t i = 0; i < Size - 1; i++) {
+        out << vector.dimensions[i] << "; ";
+    }
+
+    out << vector.dimensions[Size - 1] << ")\n";
+
+    return out;
+}
+
+template<typename Type, size_t Size>
+[[nodiscard]] constexpr Vector<Type, Size> operator+(
+    const Vector<Type, Size>& vector1,
+    const Vector<Type, Size>& vector2
+) noexcept {
+    Vector<Type, Size> resultVector {};
+
+    for (size_t i = 0; i < Size; i++) {
+        resultVector[i] = vector1[i] + vector2[i];
+    }
+
+    return resultVector;
+}
+
+template<typename Type, size_t Size>
+[[nodiscard]] constexpr Vector<Type, Size> operator-(
+    const Vector<Type, Size>& vector1,
+    const Vector<Type, Size>& vector2
+) noexcept {
+    Vector<Type, Size> resultVector {};
+
+    for (size_t i = 0; i < Size; i++) {
+        resultVector[i] = vector1[i] - vector2[i];
+    }
+
+    return resultVector;
+}
+
+template<typename Type, size_t Size>
+[[nodiscard]] constexpr Vector<Type, Size> operator*(
+    const Vector<Type, Size>& vector1,
+    const Vector<Type, Size>& vector2
+) noexcept {
+    Vector<Type, Size> resultVector {};
+
+    for (size_t i = 0; i < Size; i++) {
+        resultVector[i] = vector1[i] * vector2[i];
+    }
+
+    return resultVector;
+}
+
+template<typename Type, size_t Size>
+[[nodiscard]] constexpr Vector<Type, Size> operator*(
+    const Type& scalar,
+    const Vector<Type, Size>& vector
+) noexcept {
+    Vector<Type, Size> resultVector {};
+
+    for (size_t i = 0; i < Size; i++) {
+        resultVector[i] = scalar * vector[i];
+    }
+
+    return resultVector;
+}
+
+template<typename Type, size_t Size>
+[[nodiscard]] constexpr Vector<Type, Size> operator*(
+    const Vector<Type, Size>& vector,
+    const Type& scalar
+) noexcept {
+    return scalar * vector;
+}
+
+template<typename Type, size_t Size>
+[[nodiscard]] constexpr Vector<Type, Size> operator/(
+    const Vector<Type, Size>& vector,
+    const Type& scalar
+) noexcept {
+    return vector * (1.0F / scalar);
+}
 
 template<typename Type>
 class Vector2 : public Vector<Type, 2> {
 public:
-    constexpr Vector2(Type x = Type {}, Type y = Type {}) noexcept {
-        this->dimensions[0] = x;
-        this->dimensions[1] = y;
-    }
+    constexpr Vector2(Type x = Type {}, Type y = Type {}) noexcept :
+        Vector<Type, 2>({x, y}) {}
 
     [[nodiscard]] constexpr Type getX() const noexcept {
         return this->dimensions[0];
@@ -59,11 +213,8 @@ public:
         Type x = Type {},
         Type y = Type {},
         Type z = Type {}
-    ) noexcept {
-        this->dimensions[0] = x;
-        this->dimensions[1] = y;
-        this->dimensions[2] = z;
-    }
+    ) noexcept :
+        Vector<Type, 3>({x, y, z}) {}
 
     [[nodiscard]] constexpr Type getX() const noexcept {
         return this->dimensions[0];
@@ -73,6 +224,21 @@ public:
     }
     [[nodiscard]] constexpr Type getZ() const noexcept {
         return this->dimensions[2];
+    }
+
+    [[nodiscard]] constexpr Vector3<Type> getCrossProduct(
+        const Vector3<Type>& vector
+    ) const noexcept {
+        const Type x = this->dimensions[1] * vector.dimensions[2] -
+                       this->dimensions[2] * vector.dimensions[1];
+
+        const Type y = this->dimensions[2] * vector.dimensions[0] -
+                       this->dimensions[0] * vector.dimensions[2];
+
+        const Type z = this->dimensions[0] * vector.dimensions[1] -
+                       this->dimensions[1] * vector.dimensions[0];
+
+        return Vector3<Type> {x, y, z};
     }
 };
 
