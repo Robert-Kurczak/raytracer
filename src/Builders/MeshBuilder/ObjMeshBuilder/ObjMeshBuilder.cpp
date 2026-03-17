783ec2aa60e687bf3f533b24c75ce7201fd117f9
Robert <robertkurczak.studia@gmail.com>
2026-03-17 21:01:14 +0100

    Added parsing mtl files

--- src/Builders/MeshBuilder/ObjMeshBuilder/ObjMeshBuilder.cpp
+++ src/Builders/MeshBuilder/ObjMeshBuilder/ObjMeshBuilder.cpp
@@ -6,15 +6,104 @@
 #include "Geometry/Material/MtlMaterial/MtlMaterial.hpp"
 #include "Geometry/Material/MtlMaterial/MtlParameters.hpp"
 
+#include <filesystem>
 #include <format>
 #include <fstream>
 #include <iostream>
 #include <memory>
 #include <sstream>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 namespace RTC {
+
+MaterialsMap ObjMeshBuilder::extractMaterials(
+    const std::filesystem::path& path
+) const {
+    std::ifstream file {path};
+
+    if (!file) {
+        throw std::runtime_error(
+            std::format("Cannot open {}", path.c_str())
+        );
+    }
+
+    std::unordered_map<std::string, std::shared_ptr<IMaterial>>
+        materials_;
+
+    std::string mtlName;
+    MtlParameters mtlParameters;
+
+    std::string line;
+    while (std::getline(file, line)) {
+        std::stringstream lineStream {line};
+        std::string dataType;
+
+        lineStream >> dataType;
+
+        if (dataType == "newmtl") {
+            if (!mtlName.empty()) {
+                materials_[mtlName] =
+                    std::make_shared<MtlMaterial>(mtlParameters);
+            }
+
+            lineStream >> mtlName;
+            mtlParameters = MtlParameters {};
+        } else if (dataType == "Ka") {
+            lineStream >> mtlParameters.ambient.red;
+            lineStream >> mtlParameters.ambient.green;
+            lineStream >> mtlParameters.ambient.blue;
+        } else if (dataType == "Kd") {
+            lineStream >> mtlParameters.diffuse.red;
+            lineStream >> mtlParameters.diffuse.green;
+            lineStream >> mtlParameters.diffuse.blue;
+        } else if (dataType == "Ks") {
+            lineStream >> mtlParameters.specular.red;
+            lineStream >> mtlParameters.specular.green;
+            lineStream >> mtlParameters.specular.blue;
+        } else if (dataType == "Tr") {
+            lineStream >> mtlParameters.transparency;
+        } else if (dataType == "Ns") {
+            lineStream >> mtlParameters.shininess;
+        }
+    }
+
+    if (!mtlName.empty()) {
+        materials_[mtlName] =
+            std::make_shared<MtlMaterial>(mtlParameters);
+    }
+
+    return materials_;
+}
+
+void ObjMeshBuilder::parseMtlLib(
+    MaterialsMap& materials,
+    const std::filesystem::path& objPath,
+    std::stringstream& lineStream
+) const {
+    std::string libName;
+    lineStream >> libName;
+    const std::filesystem::path mtlPath = objPath.parent_path() / libName;
+
+    const MaterialsMap newMaterials = extractMaterials(mtlPath);
+    materials.insert(newMaterials.begin(), newMaterials.end());
+}
+
+void ObjMeshBuilder::updateUsedMaterial(
+    std::shared_ptr<IMaterial>& currentMaterial,
+    const MaterialsMap& materials,
+    std::stringstream& lineStream
+) const {
+    std::string name;
+    lineStream >> name;
+
+    if (!materials.contains(name)) {
+        std::cerr << "Ignoring undefined material: '" << name << "'\n";
+    } else {
+        currentMaterial = materials.at(name);
+    }
+}
 
 void ObjMeshBuilder::parseVertex(
     std::vector<Point3<float>>& vertexBuffer,
@@ -53,6 +142,7 @@ uint32_t ObjMeshBuilder::parseVertexIndex(
 
 void ObjMeshBuilder::parseFace(
     Mesh& mesh,
+    const std::shared_ptr<IMaterial>& material,
     const std::vector<Point3<float>>& vertexBuffer,
     std::stringstream& lineStream
 ) const {
@@ -65,15 +155,6 @@ void ObjMeshBuilder::parseFace(
 
         indices.push_back(index);
     }
-
-    MtlParameters parameters {
-        .ambient = {.red = 0.0, .green = 0.0, .blue = 0.0},
-        .diffuse = {.red = 0.7, .green = 0.1, .blue = 0.5},
-        .specular = {.red = 0.0, .green = 0.0, .blue = 0.0}
-    };
-
-    std::shared_ptr<IMaterial> material =
-        std::make_shared<MtlMaterial>(parameters);
 
     for (size_t i = 1; i + 1 < indices.size(); ++i) {
         const uint32_t fanBaseIndex = indices[0] - 1;
@@ -106,19 +187,25 @@ std::unique_ptr<Mesh> ObjMeshBuilder::buildFromFile(
     std::string line;
 
     auto mesh = std::make_unique<Mesh>();
-
     std::vector<Point3<float>> vertexBuffer {};
+
+    std::unordered_map<std::string, std::shared_ptr<IMaterial>> materials;
+    std::shared_ptr<IMaterial> currentMaterial;
 
     while (std::getline(file, line)) {
         std::stringstream lineStream {line};
-        std::string dataType;
 
+        std::string dataType;
         lineStream >> dataType;
 
         if (dataType == "v") {
             parseVertex(vertexBuffer, position, lineStream);
         } else if (dataType == "f") {
-            parseFace(*mesh, vertexBuffer, lineStream);
+            parseFace(*mesh, currentMaterial, vertexBuffer, lineStream);
+        } else if (dataType == "mtllib") {
+            parseMtlLib(materials, path, lineStream);
+        } else if (dataType == "usemtl") {
+            updateUsedMaterial(currentMaterial, materials, lineStream);
         }
     }
 
--- src/Builders/MeshBuilder/ObjMeshBuilder/ObjMeshBuilder.hpp
+++ src/Builders/MeshBuilder/ObjMeshBuilder/ObjMeshBuilder.hpp
@@ -1,12 +1,35 @@
 #pragma once
 
 #include "../IMeshBuilder.hpp"
+#include "Geometry/Material/IMaterial.hpp"
 
+#include <filesystem>
 #include <memory>
+#include <string>
+#include <unordered_map>
 
 namespace RTC {
+using MaterialsMap =
+    std::unordered_map<std::string, std::shared_ptr<IMaterial>>;
+
 class ObjMeshBuilder : public IMeshBuilder {
 private:
+    [[nodiscard]] MaterialsMap extractMaterials(
+        const std::filesystem::path& path
+    ) const;
+
+    void parseMtlLib(
+        MaterialsMap& materials,
+        const std::filesystem::path& objPath,
+        std::stringstream& lineStream
+    ) const;
+
+    void updateUsedMaterial(
+        std::shared_ptr<IMaterial>& currentMaterial,
+        const MaterialsMap& materials,
+        std::stringstream& lineStream
+    ) const;
+
     void parseVertex(
         std::vector<Point3<float>>& vertexBuffer,
         const Vector3<float>& offset,
@@ -20,6 +43,7 @@ private:
 
     void parseFace(
         Mesh& mesh,
+        const std::shared_ptr<IMaterial>& material,
         const std::vector<Point3<float>>& vertexBuffer,
         std::stringstream& lineStream
     ) const;
