a469e1b3d35692ae583de4f21a7a78d9a0c906ee
Robert <robertkurczak.studia@gmail.com>
2026-06-12 21:35:18 +0200

    Added stb_image library

---
+++ src/Rendering/Texture/ITexture.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "Core/Color/Color.hpp"
+
+namespace RTC {
+class ITexture {
+public:
+    ITexture(const ITexture&) = delete;
+    ITexture(ITexture&&) = delete;
+    ITexture& operator=(const ITexture&) = delete;
+    ITexture& operator=(ITexture&&) = delete;
+    virtual ~ITexture() = default;
+
+    [[nodiscard]] virtual LinearColor sample(float u, float v) const = 0;
+};
+}
