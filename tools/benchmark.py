6831c1c86c7a33486dd8a29bdeea1fa40451e54d
Robert Kurczak <robertkurczak.studia@gmail.com>
2026-07-31 19:57:18 +0200

    Added command line arguments to benchmark

--- tools/benchmark.py
+++ tools/benchmark.py
@@ -4,6 +4,7 @@
 Measures renderer performance
 """
 
+import argparse
 import json
 import os
 import subprocess
@@ -11,7 +12,7 @@ import sys
 import tempfile
 import time
 from dataclasses import dataclass
-from datetime import datetime
+from datetime import datetime, timezone
 from pathlib import Path
 
 from dev import common, paths
@@ -37,6 +38,15 @@ class BenchmarkResults:
     render_times: list[float]
     average_render_time: float
 
+
+@dataclass
+class Args:
+    "Command line arguments"
+
+    benchmark_name: str
+    split_renders: bool
+
+
 BENCHMARK_SCENES = [
     BenchmarkSceneData(
         resolution_x=500,
@@ -53,18 +63,20 @@ BENCHMARK_SCENES = [
         samples_per_pixel=10,
         max_recursion_depth=5,
         json_name="sponza-scene.json",
-        output_name="benchmark-sponza-sphere.exr",
+        output_name="benchmark-sponza.exr",
         renders_per_measurement=10,
     ),
 ]
 
 
-def create_benchmark_directory():
-    "Create directory for current benchmark run"
+def create_benchmark_directory(benchmark_name: str):
+    """Create directory for current benchmark run"""
 
-    timestamp = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    timestamp = datetime.now(timezone.utc).strftime(
+        "%Y-%m-%d-%H-%M-%S"
+    )
     directory_path = paths.RENDERS_DIRECTORY_PATH.joinpath(
-        f"benchmark-{timestamp}"
+        f"benchmark-{benchmark_name}-{timestamp}"
     )
 
     os.makedirs(directory_path)
@@ -123,19 +135,24 @@ def measure_render_time(scene_json_path: str):
 def prefix_output_file_name(
     output_directory: Path, output_name: str, prefix: int
 ):
-    "Prefixes rendered image name with the given number"
+    """
+    Prefixes rendered image name with the given number
+    and moves it to a separate directory
+    """
 
     prefixed_name = f"{prefix}-{output_name}"
     initial_path = output_directory.joinpath(output_name)
-    result_path = initial_path.as_posix().replace(
-        output_name, prefixed_name
-    )
+    subdirectory_path = output_directory.joinpath(initial_path.stem)
+    result_path = subdirectory_path.joinpath(prefixed_name)
 
+    os.makedirs(subdirectory_path, exist_ok=True)
     os.rename(initial_path, result_path)
 
 
 def benchmark_scene(
-    scene_data: BenchmarkSceneData, output_directory: Path
+    scene_data: BenchmarkSceneData,
+    output_directory: Path,
+    split_renders: bool,
 ):
     "Perform render time measurements on given scene"
 
@@ -166,9 +183,10 @@ def benchmark_scene(
             render_times.append(render_time)
             average_render_time += render_time
 
-            prefix_output_file_name(
-                output_directory, scene_data.output_name, i
-            )
+            if split_renders:
+                prefix_output_file_name(
+                    output_directory, scene_data.output_name, i
+                )
 
     average_render_time /= renders
 
@@ -179,11 +197,14 @@ def benchmark_scene(
 
 
 def create_benchmark_results_string(
-    scene_data: BenchmarkSceneData, results: BenchmarkResults
+    benchmark_name: str,
+    scene_data: BenchmarkSceneData,
+    results: BenchmarkResults,
 ):
     "Creats benchmark results text for given scene"
 
     return f"""
+{benchmark_name}
 === Benchmark results ===
 \tscene: {scene_data.json_name}
 \twidth: {scene_data.resolution_x}
@@ -197,6 +218,7 @@ def create_benchmark_results_string(
 
 
 def export_benchmark_results(
+    benchmark_name: str,
     scene_data: BenchmarkSceneData,
     results: BenchmarkResults,
     output_directory: Path,
@@ -204,7 +226,7 @@ def export_benchmark_results(
     "Exports and logs benchmark results"
 
     results_string = create_benchmark_results_string(
-        scene_data, results
+        benchmark_name, scene_data, results
     )
 
     render_times_string = ", \n".join(
@@ -220,16 +242,57 @@ def export_benchmark_results(
         file.write(f"Render times: [\n{render_times_string}\n]")
 
 
-def main():
+def start_benchmark(args: Args):
     "Main benchmark loop"
 
-    output_directory = create_benchmark_directory()
+    output_directory = create_benchmark_directory(args.benchmark_name)
 
     for scene_data in BENCHMARK_SCENES:
-        results = benchmark_scene(scene_data, output_directory)
-        export_benchmark_results(scene_data, results, output_directory)
+        results = benchmark_scene(
+            scene_data, output_directory, args.split_renders
+        )
+        export_benchmark_results(
+            args.benchmark_name, scene_data, results, output_directory
+        )
 
     common.log_success("=== Benchmark successfull ===")
+
+
+def parse_args() -> Args:
+    """Parser command line arguments"""
+
+    parser = argparse.ArgumentParser(
+        prog="Benchmark",
+        description="Benchmarks raytracer performance",
+    )
+
+    parser.add_argument(
+        "-n",
+        "--name",
+        type=str,
+        required=False,
+        default="",
+        help="Optional name for benchmark run",
+    )
+
+    parser.add_argument(
+        "-s",
+        "--split",
+        action="store_true",
+        required=False,
+        help="Make each rendered image during measurement a separate file",
+    )
+
+    parsed = parser.parse_args()
+
+    return Args(benchmark_name=parsed.name, split_renders=parsed.split)
+
+
+def main():
+    "Main script function"
+    args = parse_args()
+    start_benchmark(args)
+
 
 if __name__ == "__main__":
     main()
